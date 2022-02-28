6809bf313ce7d3d875ae3203743a2f040f9d5850
Leo Unglaub <leo@unglaub.at>
2022-02-28 22:42:51 +0100

    Adding the basic version of the extension

---
+++ src/LaravelBaselineServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LeoUnglaub\LaravelBaseline;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBaselineServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+
+	public function boot()
+	{
+	}
+}
