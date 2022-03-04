cf5d8a9b7b07aa70f1f43f82db4bf37672e7dbd6
Leo Unglaub <leo@unglaub.at>
2022-03-04 19:35:56 +0100

    Use the new php-cs-config from leo-unglaub/laravel-baseline-dev

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,10 @@
 <?php
 
-use LeoUnglaub\LaravelBaseline\PhpCsFixer\Config;
+/**
+ * The default config for all our projects. No forther config needed.
+ *
+ * @version 0.2.0
+ * @see https://code.foxship.com/foxship/server
+ */
 
-return new Config();
+return new LeoUnglaub\LaravelBaselineDev\PhpCsFixer\Config();
