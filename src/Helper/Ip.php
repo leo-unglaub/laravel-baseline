0982ecb31fc1a8f15652b33b84174440412ea03b
Leo Unglaub <leo@unglaub.at>
2022-03-04 19:42:16 +0100

    Adding a Ip helper class

---
+++ src/Helper/Ip.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LeoUnglaub\LaravelBaseline\Helper;
+
+/**
+ * A helper class for managing IP addresses.
+ */
+class Ip
+{
+	protected string $ip = null;
+
+	/**
+	 * Create a new Ip instane.
+	 *
+	 * @return void
+	 */
+	public function __construct(string $ip = null)
+	{
+		$this->ip = $ip;
+	}
+
+	/**
+	 * Return true if the address is a valid IPv4 address.
+	 *
+	 * @TODO: This is a very simple check, maybe it can be extended to a Regex
+	 */
+	public function isV4(): bool
+	{
+		if (str_contains($this->ip, '.'))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Return true if the address is a valid IPv6 address.
+	 *
+	 * @TODO: This is a very simple check, maybe it can be extended to a Regex
+	 */
+	public function isV6(): bool
+	{
+		if (str_contains($this->ip, ':'))
+		{
+			return true;
+		}
+
+		return false;
+	}
+}
